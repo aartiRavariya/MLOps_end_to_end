89c6bb602829067486f9d9bd308d1a9c765623d2
aartiRavariya <patelaiysha.py@gmail.com>
2026-07-26 02:14:57 +0530

    fixed dagshub authorization

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -14,25 +14,26 @@ warnings.filterwarnings("ignore")
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
 # Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+dagshub_token = os.getenv("YT_CAPSTONE_PROJECT")
+if not dagshub_token:
+    raise EnvironmentError("YT_CAPSTONE_PROJECT environment variable is not set")
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "vikashdas770"
-# repo_name = "YT-Capstone-Project"
-# # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+dagshub_url = "https://dagshub.com"
+repo_owner = "aartiRavariya"
+repo_name = "MLOps_end_to_end"
+
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 # -------------------------------------------------------------------------------------
 
 
 # Below code block is for local use
 # -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri('https://dagshub.com/aartiRavariya/MLOps_end_to_end.mlflow')
-dagshub.init(repo_owner='aartiRavariya', repo_name='MLOps_end_to_end', mlflow=True)
+# mlflow.set_tracking_uri('https://dagshub.com/aartiRavariya/MLOps_end_to_end.mlflow')
+# dagshub.init(repo_owner='aartiRavariya', repo_name='MLOps_end_to_end', mlflow=True)
 # -------------------------------------------------------------------------------------
 
 
